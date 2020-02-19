e3420c092c28800b965470a0c6a7a3bdfee27668
kentsx <admin@kmore.ml>
2020-02-19 17:28:03 +0800

    通过设置了STATIC部分，解决了CSS问题 主要原因在于static文件夹要放在app的根目录

--- mdn/mdn/settings.py
+++ mdn/mdn/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
